1f0893688d69062a081c2b91541f4af2a4424c92
Dipson419 <165921074+Dipson419@users.noreply.github.com>
2025-10-03 08:54:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'SASUKE 1M?', // Your name
+  ownerNumber: '2349065006215', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
